34e8deceefd1fc9f749eca182d76c3b5bed37b95
MindyYT <mindynicolep@googlemail.com>
2017-01-14 21:07:00 +0100

    Create L_PortalTracker.java

---
+++ Listener/L_PortalTracker.java
@@ -0,0 +1,100 @@
+package Listener;
+
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import me.mindyyt.PortalRun.PortalRun;
+import net.md_5.bungee.api.ChatColor;
+
+public class L_PortalTracker implements Listener{
+
+	// Instanz setzen für die Maineinbindung
+	private PortalRun plugin;
+	public L_PortalTracker(PortalRun pr){
+		pr = plugin;
+	}
+	
+	@EventHandler
+	public void onCompassInteract(PlayerInteractEvent e){
+		
+		// Check, if Player has compass in hand
+		Player p = e.getPlayer();
+		
+		if(p.getItemInHand().getType() == Material.COMPASS){
+			
+			// Check if the player made a rightclick
+			if(e.getAction() == Action.RIGHT_CLICK_AIR){
+				
+				// Get the location of the player
+				Location loc = p.getLocation();
+				loc.setY(loc.getY() + 1);
+				
+				// Set the min range around the player
+				int minX = p.getLocation().getBlockX() - 50;
+				int minY = p.getLocation().getBlockY() - 25;
+				int minZ = p.getLocation().getBlockZ() - 50;
+				
+				// Set the max range around the player
+				int maxX = p.getLocation().getBlockX() + 50;
+				int maxY = p.getLocation().getBlockY() + 25;
+				int maxZ = p.getLocation().getBlockZ() + 50;
+				
+				// For loops for "scanning" the blocks
+				for(int x = minX; x <= maxX; x++){
+					for(int y = minY; y <= maxY; y++){
+						for(int z = minZ; z <= maxZ; z++){
+							
+							// Check if there is an beacon in this area
+							Location locBlock = new Location(p.getWorld(),x,y,z);
+							Block beacon = p.getWorld().getBlockAt(locBlock);
+							
+							// Output for the player
+							if(beacon.getType() == Material.BEACON){
+								
+								// Check Playerpositon
+								int pLocX = p.getLocation().getBlockX();
+								int pLocZ = p.getLocation().getBlockZ();
+								
+								// Check Block location
+								int bLocX = beacon.getLocation().getBlockX();
+								int bLocZ = beacon.getLocation().getBlockZ();
+								
+								// Get the difference between player and beacon
+								
+								// X Axis
+								if(pLocX - bLocX > 40 || -(pLocX) - (-bLocX) > 40){
+									p.sendMessage(ChatColor.RED + "There is a portal nearby");
+								}else if(pLocX - bLocX <= 40 && pLocX - bLocX > 25 || (-pLocX) - (-bLocX) <= 40 && (-pLocX) - (-bLocX) > 25 ){
+									p.sendMessage(ChatColor.YELLOW + "There is a portal nearby");
+								}else{
+									p.sendMessage(ChatColor.GREEN + "There is a portal nearby");
+								}
+								
+								// Z Axis
+								if(pLocZ - bLocZ > 40 || -(pLocZ) - (-bLocZ) > 40){
+									p.sendMessage(ChatColor.RED + "There is a portal nearby");
+								}else if(pLocZ - bLocZ <= 40 && pLocZ - bLocZ > 25 || (-pLocZ) - (-bLocZ) <= 40 && (-pLocZ) - (-bLocZ) > 25 ){
+									p.sendMessage(ChatColor.YELLOW + "There is a portal nearby");
+								}else{
+									p.sendMessage(ChatColor.GREEN + "There is a portal nearby");
+								}
+							}
+						}
+					}
+				}
+				
+				
+				// Ausgabe zum Test
+				// p.sendMessage(" " + loc.getY());
+				
+			}
+		}
+	}	
+}
